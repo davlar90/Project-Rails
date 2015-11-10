1ec65cbe7ab45f1ef6a1ccfdbaf55c2c0c559c7c
David Larsson <david_mollosund@hotmail.com>
2015-11-10 14:25:49 +0100

    Tagging feature completed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,11 @@
 module ArticlesHelper
-def article_params
-  params.require(:article).permit(:title, :body)
-end
+
+
+		def article_params
+		params.require(:article).permit(:title, :body, :tag_list)
+	end
 
 
 end
 
+ 
